f65a72e8427aec89522dee20ce22a9db4f28c515
Bryan Triana <bryantrianarueda@gmail.com>
2018-12-25 10:14:08 -0500

    Added initial entrypoint

---
+++ StudyOrder/Application.java
@@ -0,0 +1,15 @@
+/*
+* Copyright (c) 2018 Inverse Palindrome
+* StudyOrder - Application.java
+* https://inversepalindrome.com/
+*/
+
+
+package com.inversepalindrome.studyorder;
+
+
+public class Application {
+    public static void main(String[] args){
+
+    }
+}
