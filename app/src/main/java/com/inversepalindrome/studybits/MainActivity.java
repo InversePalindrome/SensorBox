075d95aecc44f537e01b4d9456c4081868f2d455
Bryan Triana <bryantrianarueda@gmail.com>
2018-12-26 19:26:05 -0500

    Initial commit

---
+++ app/src/main/java/com/inversepalindrome/studybits/MainActivity.java
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2018 Inverse Palindrome
+* StudyBits - MainActivity.java
+* https://inversepalindrome.com/
+*/
+
+
+package com.inversepalindrome.studybits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
