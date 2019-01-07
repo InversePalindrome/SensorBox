90a992bde75b55a40682d223fadb2910ffe0eaa5
Bryan Triana <bryantrianarueda@gmail.com>
2019-01-06 21:55:48 -0500

    Added transition to compass activity

--- app/src/main/java/com/inversepalindrome/studybits/MainActivity.java
+++ app/src/main/java/com/inversepalindrome/studybits/MainActivity.java
@@ -8,7 +8,10 @@
 package com.inversepalindrome.studybits;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.CompassButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent compassIntent = new Intent(MainActivity.this, CompassActivity.class);
+                startActivity(compassIntent);
+            }
+        });
     }
 }
